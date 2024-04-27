a311fa51ae547e5c9c9c549aaf51221a909f34fe
John Peterson <johnp5419@gmail.com>
2024-04-27 15:18:11 -0600

    Initializing Vulkan Repo

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+#include <stdexcept>
+
+class VulkanApp 
+{
+public:
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
+private:
+    GLFWwindow* window;
+
+public:
+    void run()
+    {
+        initWindow();
+        mainLoop();
+    }
+
+private:
+    void initWindow() 
+    {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+    }
+
+    void mainLoop() 
+    {
+        while (!glfwWindowShouldClose(window)) 
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void initVulkan()
+    {
+        
+    }
+};
+
+int main() 
+{
+    VulkanApp app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
