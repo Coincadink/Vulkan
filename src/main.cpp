eabfb0dc1b35683271721e8475d61724c187e9b5
John Peterson <johnp5419@gmail.com>
2024-06-30 00:39:04 -0600

    Updating glm

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ private:
     void initWindow() 
     {
         glfwInit();
-
+        
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
